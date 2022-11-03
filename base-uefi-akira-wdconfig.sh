3482c18434982ba112c9ac4f44bbeec8b0d4b04e
hiddenblade99 <116996916+hiddenblade99@users.noreply.github.com>
2022-11-03 10:59:27 +0530

    Update base-uefi-akira-wdconfig.sh

--- base-uefi-akira-wdconfig.sh
+++ base-uefi-akira-wdconfig.sh
@@ -21,7 +21,7 @@ echo "127.0.0.1 localhost" >> /etc/hosts
 echo "::1       localhost" >> /etc/hosts
 echo "127.0.1.1 pangea.localdomain pangea" >> /etc/hosts
 echo root:password | chpasswd
-pacman -S --noconfirm grub efibootmgr networkmanager dialog wpa_supplicant mtools dosfstools base-devel linux-headers pulseaudio os-prober ntfs-3g firefox mpv tlp ttf-jetbrains-mono thunar engrampa telegram-desktop pavucontrol neofetch mousepad git
+pacman -S --noconfirm grub efibootmgr networkmanager dialog wpa_supplicant mtools dosfstools base-devel linux-headers pulseaudio os-prober ntfs-3g firefox mpv mupdf sxiv tlp tlp-rdw ttf-jetbrains-mono thunar engrampa telegram-desktop pavucontrol mousepad git
 pacman -S --noconfirm xf86-video-amdgpu
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB 
 grub-mkconfig -o /boot/grub/grub.cfg
